7d5e50479d8a29566bbc64ec1e27a562e0db12a9
qqjt <itkid@live.com>
2018-04-18 21:26:28 +0800

    :bug: fix toc bug

--- app/Observers/PostObserver.php
+++ app/Observers/PostObserver.php
@@ -14,50 +14,48 @@ class PostObserver
     {
         $markdown = new Markdown();
         $html = $markdown->convertMarkdownToHtml($post->content);
-        if (mb_strlen($html) > 2048) {
-            //add anchor in html && generate TOC
-            $htmlDom = HtmlDomParser::str_get_html($html);
-            $headings = $htmlDom->find('h2,h3,h4,h5');
-            $toc = '<ul>';
-            $level = 2;
-            foreach ($headings as $key => $heading) {
-                $headingHtml = $heading->outertext();
-                $curLevel = (int)$headingHtml[2];
-                $anchor = 'h' . $curLevel . '-' . $key;
-                $heading->id = $anchor;
-                if ($curLevel == $level) {
-                    if ($key != 0)
-                        $toc .= '</li>';
-                    $toc .= '<li><a href="#' . $anchor . '">' . $heading->innertext() . '</a>';
-                } elseif ($curLevel > $level) {
-                    $toc .= '</li><ul>';
+
+        //add anchor in html && generate TOC
+        $htmlDom = HtmlDomParser::str_get_html($html);
+        $headings = $htmlDom->find('h2,h3,h4,h5');
+        $toc = '<ul>';
+        $level = 2;
+        $curLevel = 2;
+        foreach ($headings as $key => $heading) {
+            $headingHtml = $heading->outertext();
+            $curLevel = (int)$headingHtml[2];
+            $anchor = 'h' . $curLevel . '-' . $key;
+            $heading->id = $anchor;
+            if ($curLevel == $level) {
+                if ($key != 0)
+                    $toc .= '</li>';
+                $toc .= '<li><a href="#' . $anchor . '">' . $heading->innertext() . '</a>';
+            } elseif ($curLevel > $level) {
+                $toc .= '</li><ul>';
+                $level += 1;
+                while ($curLevel > $level) {
+                    $toc .= '<li><ul>';
                     $level += 1;
-                    while ($curLevel > $level) {
-                        $toc .= '<li><ul>';
-                        $level += 1;
-                    }
-                    $toc .= '<li><a href="#' . $anchor . '">' . $heading->innertext() . '</a>';
-                } else {
-                    $toc .= '</li></ul>';
-                    $level -= 1;
-                    while ($curLevel < $level) {
-                        $toc .= '</li></ul>';
-                        $level -= 1;
-                    }
-                    $toc .= '<li><a href="#' . $anchor . '">' . $heading->innertext() . '</a>';
                 }
-            }
-            while ($curLevel > $level) {
+                $toc .= '<li><a href="#' . $anchor . '">' . $heading->innertext() . '</a>';
+            } else {
                 $toc .= '</li></ul>';
                 $level -= 1;
+                while ($curLevel < $level) {
+                    $toc .= '</li></ul>';
+                    $level -= 1;
+                }
+                $toc .= '<li><a href="#' . $anchor . '">' . $heading->innertext() . '</a>';
             }
-            $toc .= '</ul>';
-
-            $post->toc = $toc;
-            $post->html = (string)$htmlDom;
-        } else {
-            $post->html = $html;
         }
+        while ($curLevel > $level) {
+            $toc .= '</li></ul>';
+            $curLevel -= 1;
+        }
+        $toc .= '</ul>';
+
+        $post->toc = $toc;
+        $post->html = (string)$htmlDom;
         $post->excerpt = str_limit(strip_tags($post->html), 300);
     }
 
