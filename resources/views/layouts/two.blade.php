eddcdd1fdcc7f796297d7f4eec4335ffe394ca43
qqjt <itkid@live.com>
2018-05-07 22:15:56 +0800

    :art: fix layout

--- resources/views/layouts/two.blade.php
+++ resources/views/layouts/two.blade.php
@@ -20,67 +20,66 @@
 </head>
 <body>
 <nav class="navbar navbar-expand-md navbar-dark flex-column flex-md-row bd-navbar bg-primary">
-    <div class="container">
-        <a class="navbar-brand" href="{{ url('/') }}">
-            {{ config('app.name', 'Laravel') }}
-        </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <!-- Left Side Of Navbar -->
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item {{active_class(if_route('archive'))}}">
-                    <a class="nav-link" href="{{ route('archive') }}"><i
-                                class="fa fa-archive"></i>&nbsp;{{__("Archive")}}
+    <a class="navbar-brand" href="{{ url('/') }}">
+        {{ config('app.name', 'Laravel') }}
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <!-- Left Side Of Navbar -->
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item {{active_class(if_route('archive'))}}">
+                <a class="nav-link" href="{{ route('archive') }}"><i
+                            class="fa fa-archive"></i>&nbsp;{{__("Archive")}}
+                </a>
+            </li>
+            <li class="nav-item {{active_class(if_route('about'))}}">
+                <a class="nav-link" href="{{ route('about') }}"><i class="fa fa-user"></i>&nbsp;{{__("About")}}
+                </a>
+            </li>
+        </ul>
+        <!-- Right Side Of Navbar -->
+        <ul class="navbar-nav ml-auto">
+            <!-- Authentication Links -->
+            @guest
+                <li class="{{active_class(if_route('login'))}}"><a class="nav-link" href="{{ route('login') }}"><i
+                                class="fa fa-sign-in"
+                                aria-hidden="true"></i>&nbsp;{{__("Login")}}
+                    </a></li>
+                <li class="{{active_class(if_route('register'))}}"><a class="nav-link"
+                                                                      href="{{ route('register') }}"><i
+                                class="fa fa-user-plus"
+                                aria-hidden="true"></i>&nbsp;{{__("Register")}}
+                    </a></li>
+            @else
+                <li class="{{active_class(if_route('post.create'))}}"><a class="nav-link"
+                                                                         href="{{ route('post.create') }}"><i
+                                class="fa fa-plus"></i>&nbsp;{{__("New")}}</a></li>
+                <li class="nav-item dropdown">
+                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                       data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        {{ Auth::user()->name }} <span class="caret"></span>
                     </a>
-                </li>
-                <li class="nav-item {{active_class(if_route('about'))}}">
-                    <a class="nav-link" href="{{ route('about') }}"><i class="fa fa-user"></i>&nbsp;{{__("About")}}
-                    </a>
-                </li>
-            </ul>
-            <!-- Right Side Of Navbar -->
-            <ul class="navbar-nav ml-auto">
-                <!-- Authentication Links -->
-                @guest
-                    <li class="{{active_class(if_route('login'))}}"><a class="nav-link" href="{{ route('login') }}"><i
-                                    class="fa fa-sign-in"
-                                    aria-hidden="true"></i>&nbsp;{{__("Login")}}
-                        </a></li>
-                    <li class="{{active_class(if_route('register'))}}"><a class="nav-link"
-                                                                          href="{{ route('register') }}"><i
-                                    class="fa fa-user-plus"
-                                    aria-hidden="true"></i>&nbsp;{{__("Register")}}
-                        </a></li>
-                @else
-                    <li class="{{active_class(if_route('post.create'))}}"><a class="nav-link"
-                                                                             href="{{ route('post.create') }}"><i
-                                    class="fa fa-plus"></i>&nbsp;{{__("New")}}</a></li>
-                    <li class="nav-item dropdown">
-                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                           data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            {{ Auth::user()->name }} <span class="caret"></span>
+
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="{{ route('logout') }}"
+                           onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                            {{__("Logout")}}
                         </a>
 
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="{{ route('logout') }}"
-                               onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                {{__("Logout")}}
-                            </a>
-
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST"
-                                  style="display: none;">
-                                @csrf
-                            </form>
-                        </div>
-                    </li>
-                @endguest
-            </ul>
-        </div>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                              style="display: none;">
+                            @csrf
+                        </form>
+                    </div>
+                </li>
+            @endguest
+        </ul>
     </div>
 </nav>
 
