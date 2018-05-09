ded45c4cad3bef3261a235f33c64d42cbeef34d8
qqjt <itkid@live.com>
2018-05-09 17:59:35 +0800

    :heavy_plus_sign: add http cache again, for real.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -58,5 +58,7 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'http.cache' => \Barryvdh\HttpCache\Middleware\CacheRequests::class,
+        'ttl' => \Barryvdh\HttpCache\Middleware\SetTtl::class,
     ];
 }
---
+++ config/httpcache.php
@@ -0,0 +1,37 @@
+<?php
+
+return array(
+
+    /*
+     |--------------------------------------------------------------------------
+     | HttpCache Settings
+     |--------------------------------------------------------------------------
+     |
+     | Enable the HttpCache to cache public resources, with a shared max age (or TTL)
+     | Enable ESI for edge side includes (parts that can be cached separate)
+     | Set the cache to a writable dir, outside the document root.
+     |
+     */
+    'enabled' => true,
+    'esi' => false,
+    'cache_dir' => storage_path('httpcache'),
+
+    /*
+     |--------------------------------------------------------------------------
+     | Extra options
+     |--------------------------------------------------------------------------
+     |
+     | Configure the default HttpCache options. See for a list of options:
+     | http://symfony.com/doc/current/book/http_cache.html#symfony2-reverse-proxy
+     |
+     */
+    'options' => [
+        'debug'                  => env('APP_DEBUG', false),
+        'default_ttl'            => 60,
+        'private_headers'        => ['Authorization'],
+        'allow_reload'           => true, // "ctrl+shift+r" force refresh
+        'allow_revalidate'       => true, // "ctrl+r" refresh button
+        'stale_while_revalidate' => 2,
+        'stale_if_error'         => 60,
+    ],
+);
