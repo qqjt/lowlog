f888f2e37cd302d3cf0a45661dad94ffef563e19
qqjt <itkid@live.com>
2018-05-10 17:03:26 +0800

    :wrench: add google adsense

--- resources/views/elements/analytics.blade.php
+++ resources/views/elements/analytics.blade.php
@@ -7,6 +7,13 @@
 
     gtag('config', 'UA-51677064-1');
 </script>
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script>
+    (adsbygoogle = window.adsbygoogle || []).push({
+        google_ad_client: "ca-pub-9451565495816715",
+        enable_page_level_ads: true
+    });
+</script>
 <!--百度统计-->
 <script>
     var _hmt = _hmt || [];
