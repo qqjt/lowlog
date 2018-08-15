1aff70610f721a967d13adcbe314fcff95323a24
qqjt <itkid@live.com>
2018-08-15 10:56:40 +0800

    resolved #2 change post paging

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,39 +1,1 @@
-@servers(['prod' => '-p 233 deployer@low.bi'])
-
-@setup
-    $repository = 'git@git.codegeass.cc:qqjt/lowlog.git';
-    $releases_dir = '/var/www/lowlog/releases';
-    $app_dir = '/var/www/lowlog';
-    $release = date('YmdHis');
-    $new_release_dir = $releases_dir .'/'. $release;
-@endsetup
-
-@story('deploy')
-    clone_repository
-    run_composer
-    update_symlinks
-@endstory
-
-@task('clone_repository')
-    echo 'Cloning repository'
-    [ -d {{ $releases_dir }} ] || mkdir {{ $releases_dir }}
-    git clone --depth 1 {{ $repository }} {{ $new_release_dir }}
-@endtask
-
-@task('run_composer')
-    echo "Starting deployment ({{ $release }})"
-    cd {{ $new_release_dir }}
-    composer install --prefer-dist --no-scripts -q -o
-@endtask
-
-@task('update_symlinks')
-    echo "Linking storage directory"
-    rm -rf {{ $new_release_dir }}/storage
-    ln -nfs {{ $app_dir }}/storage {{ $new_release_dir }}/storage
-
-    echo 'Linking .env file'
-    ln -nfs {{ $app_dir }}/.env {{ $new_release_dir }}/.env
-
-    echo 'Linking current release'
-    ln -nfs {{ $new_release_dir }} {{ $app_dir }}/current
-@endtask
+@servers(['prod' => '-p 233 deployer@low.bi'])@setup    $repository = 'git@git.codegeass.cc:qqjt/lowlog.git';    $releases_dir = '/var/www/lowlog/releases';    $app_dir = '/var/www/lowlog';    $release = date('YmdHis');    $new_release_dir = $releases_dir .'/'. $release;@endsetup@story('deploy')    clone_repository    run_composer    update_symlinks@endstory@task('clone_repository')    echo 'Cloning repository'    [ -d {{ $releases_dir }} ] || mkdir {{ $releases_dir }}    git clone --depth 1 {{ $repository }} {{ $new_release_dir }}@endtask@task('run_composer')    echo "Starting deployment ({{ $release }})"    cd {{ $new_release_dir }}    composer install --prefer-dist --no-scripts -q -o@endtask@task('update_symlinks')    echo "Linking storage directory"    rm -rf {{ $new_release_dir }}/storage    ln -nfs {{ $app_dir }}/storage {{ $new_release_dir }}/storage    echo 'Linking uploads directory'    ln -nfs {{ $app_dir }}/public/uploads {{ $new_release_dir }}/public/uploads    echo 'Linking .env file'    ln -nfs {{ $app_dir }}/.env {{ $new_release_dir }}/.env    echo 'Linking current release'    ln -nfs {{ $new_release_dir }} {{ $app_dir }}/current@endtask
