227d0722cf300ef5e2bfb406ffed37a2bbd1d7ca
qqjt <itkid@live.com>
2018-08-24 14:02:24 +0800

    add home page profile

--- config/blog.php
+++ config/blog.php
@@ -3,6 +3,7 @@ return [
     'blogger' => [
         'name' => env('BLOGGER_NAME'),
         'email' => env('BLOGGER_EMAIL'),
-        'password' => env('BLOGGER_PASSWORD')
+        'password' => env('BLOGGER_PASSWORD'),
+        'desc'=> env('BLOGGER_DESC')
     ]
 ];
--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -1,12 +1,49 @@
-@extends('layouts.app')
+@extends('layouts.two')
+@section('sidebar')
+    <div class="profile-sidebar text-center d-none d-md-block">
+        <!-- SIDEBAR USERPIC -->
+        <div class="profile-avatar">
+            <img src="{{url('avatar.jpg')}}" class="rounded-circle" alt="">
+        </div>
+        <!-- END SIDEBAR USERPIC -->
+        <!-- SIDEBAR USER TITLE -->
+        <div class="profile-title py-1">
+            <div class="profile-title-name">
+                {{config('blog.blogger.name')}}
+            </div>
+            <div class="profile-title-job">
+                {{config('blog.blogger.desc')}}
+            </div>
+        </div>
+        <!-- END SIDEBAR USER TITLE -->
+        <!-- SIDEBAR BUTTONS -->
+        <div class="profile-buttons">
+            <button type="button" class="btn btn-primary btn-sm"><i class="fa fa-twitter"></i></button>
+            <button type="button" class="btn btn-danger btn-sm"><i class="fa fa-envelope"></i></button>
+        </div>
+        <!-- END SIDEBAR BUTTONS -->
+        <!-- SIDEBAR MENU -->
+        <nav class="profile-menu py-2 mx-3">
+            <div class="list-group list-group-flush">
+                <hr>
+                <a href="{{route('post.index')}}" class="list-group-item list-group-item-action">
+                    <i class="fa fa-home"></i>&nbsp;{{__("Home")}}
+                </a>
+                <a href="https://github.com/qqjt" target="_blank" class="list-group-item list-group-item-action">
+                    <i class="fa fa-github"></i>&nbsp;qqjt <i class="fa fa-external-link"></i>
+                </a>
+                <hr>
+            </div>
+        </nav>
+        <!-- END MENU -->
+    </div>
+@endsection
 @section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-10">
+    <div class="col-md-9">
                 @if(count($paginatedPosts))
                     <ul class="list-unstyled mb-0">
                         @foreach($paginatedPosts as $post)
-                            <li class="media mb-3">
+                            <li class="media pt-3">
                                 <div class="media-body">
                                     <h4 class="media-heading"><a
                                                 href="{{route('post.show', ['hashid'=> $post->hashid])}}">{{$post->title}}</a>
@@ -49,9 +86,21 @@
                     {{__('Nothing here.')}}
                 @endif
             </div>
-        </div>
-    </div>
 @endsection
 @section('style')
     @include('elements.analytics')
+    <style>
+        .sidebar {
+            border-right: 1px solid rgba(0,0,0,.1);
+        }
+        .profile-avatar img {
+            float: none;
+            margin: 0 auto;
+            width: 50%;
+            height: 50%;
+            -webkit-border-radius: 50% !important;
+            -moz-border-radius: 50% !important;
+            border-radius: 50% !important;
+        }
+    </style>
 @endsection
