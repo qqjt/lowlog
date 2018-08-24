856792450196b54ae081ee6767a7bffe86109faf
qqjt <itkid@live.com>
2018-08-24 14:30:05 +0800

    fix post index page

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -39,60 +39,60 @@
     </div>
 @endsection
 @section('content')
-    <div class="col-md-9">
-                @if(count($paginatedPosts))
-                    <ul class="list-unstyled mb-0">
-                        @foreach($paginatedPosts as $post)
-                            <li class="media pt-3">
-                                <div class="media-body">
-                                    <h4 class="media-heading"><a
-                                                href="{{route('post.show', ['hashid'=> $post->hashid])}}">{{$post->title}}</a>
-                                    </h4>
-                                    <p>{{$post->excerpt}}</p>
-                                    <ul class="list-inline">
-                                        <li class="list-inline-item">
-                                            <span><i class="fa fa-calendar"></i> {{(string)$post->posted_at}}</span>
-                                        </li>
-                                        @if(!$post->categories->isEmpty())
-                                            <li class="list-inline-item">
+
+    @if(count($paginatedPosts))
+        <ul class="list-unstyled mb-0">
+            @foreach($paginatedPosts as $post)
+                <li class="media pt-3">
+                    <div class="media-body">
+                        <h4 class="media-heading"><a
+                                    href="{{route('post.show', ['hashid'=> $post->hashid])}}">{{$post->title}}</a>
+                        </h4>
+                        <p>{{$post->excerpt}}</p>
+                        <ul class="list-inline">
+                            <li class="list-inline-item">
+                                <span><i class="fa fa-calendar"></i> {{(string)$post->posted_at}}</span>
+                            </li>
+                            @if(!$post->categories->isEmpty())
+                                <li class="list-inline-item">
                                                 <span><i class="fa fa-folder"></i>
                                                     @foreach($post->categories as $category)
                                                         <a href="{{route('post.index', ['cat'=>$category->name])}}">{{$category->name}}</a>
                                                         @if(!$loop->last),@endif
                                                     @endforeach
                                                 </span>
-                                            </li>
-                                        @endif
-                                        @if(!$post->tags->isEmpty())
-                                            <li class="list-inline-item">
-                                                <span><i class="fa fa-tags"></i></span>
-                                                @foreach($post->tags as $tag)
-                                                    <a href="{{route('post.index', ['tags'=>$tag->tag_value])}}"><span
-                                                                class="badge badge-primary">{{$tag->display_name}}</span></a>
-                                                @endforeach
-                                            </li>
-                                        @endif
-                                    </ul>
-                                </div>
-                            </li>
-                        @endforeach
-                    </ul>
-                    <div class="text-center">
-                        <nav>
-                            {{$paginatedPosts->links()}}
-                        </nav>
+                                </li>
+                            @endif
+                            @if(!$post->tags->isEmpty())
+                                <li class="list-inline-item">
+                                    <span><i class="fa fa-tags"></i></span>
+                                    @foreach($post->tags as $tag)
+                                        <a href="{{route('post.index', ['tags'=>$tag->tag_value])}}"><span
+                                                    class="badge badge-primary">{{$tag->display_name}}</span></a>
+                                    @endforeach
+                                </li>
+                            @endif
+                        </ul>
                     </div>
-                @else
-                    {{__('Nothing here.')}}
-                @endif
-            </div>
+                </li>
+            @endforeach
+        </ul>
+        <div class="text-center">
+            <nav>
+                {{$paginatedPosts->links()}}
+            </nav>
+        </div>
+    @else
+        {{__('Nothing here.')}}
+    @endif
 @endsection
 @section('style')
     @include('elements.analytics')
     <style>
         .sidebar {
-            border-right: 1px solid rgba(0,0,0,.1);
+            border-right: 1px solid rgba(0, 0, 0, .1);
         }
+
         .profile-avatar img {
             float: none;
             margin: 0 auto;
